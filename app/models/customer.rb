32be12be1948a4edd0d934181a0dbee4d31ad337
bmayes@gmail.com <bmayes@gmail.com>
2013-03-26 15:41:40 -0700

    Example repo for testing ActiveResource.

---
+++ app/models/customer.rb
@@ -0,0 +1,3 @@
+class Customer < ActiveResource::Base
+  self.site = "http://localhost:3001"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestAres::Application.config.secret_token = '59112a825e451c7b28f8c43c74bb3e4a9a772f58afcde8e0557851a45beff798a682064ccf0f36b67375f4b52d82584997229595d19f3a3de6a877cef8e18a2a'
